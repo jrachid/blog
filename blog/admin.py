9d901000130ac73ed6ccd841c653cb07834825b7
jrachid <rachidj@protonmail.ch>
2019-01-19 23:10:34 +0100

    Comments adding

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Post 
+from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 	list_display = ('title', 'slug', 'author', 'publish','status')
 	prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('name','post','created','active')
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post 
+from .models import Post, Comment 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -27,11 +28,33 @@ def post_detail(request, year, month, day, post):
 								publish__year=year,
 								publish__month=month,
 								publish__day=day)
-	return render(request, 'blog/post/detail.html', {'post':post})
+	# List of active comments for this post
+	comments = post.comments.filter(active=True)
+
+	new_comment = None
+
+	if request.method == 'POST':
+		# A comment was posted
+		comment_form = CommentForm(data=request.POST)
+		if comment_form.is_valid():
+			# create comment object but don't save to db yet
+			new_comment = comment_form.save(commit=False)
+			# Assign the current post to the comment
+			new_comment.post = post 
+			# Save the comment to db
+			new_comment.save()
+	else:
+		comment_form = CommentForm()
+
+	return render(request, 'blog/post/detail.html', {'post':post, 
+													'comments':comments, 
+													'new_comment':new_comment,
+													'comment_form':comment_form})
 
 def post_share(request, post_id):
 	# retrieve post by id
 	post = get_object_or_404(Post, id=post_id, status='published')
+	sent = False 
 
 	if request.method == 'POST':
 		# Form was submitted
@@ -39,11 +62,16 @@ def post_share(request, post_id):
 		if form.is_valid():
 			# form fields passed validation
 			cd = form.cleaned_data
+			post_url = request.build_absolute_uri(post.get_absolute_url())
+			subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+			message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+			send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+			sent = True 
 	else:
 		form = EmailPostForm()
 
-	return render(request, 'blog/post/share.html', {'post': post, 'form':form})
-	
+	return render(request, 'blog/post/share.html', {'post': post, 'form':form, 'sent':sent})
+
 
 class PostListView(ListView):
 	queryset = Post.published.all()

54d6348801dd16f59ea2ac7fdfac61dcc0b58b37
jrachid <rachidj@protonmail.ch>
2019-01-22 15:51:26 +0100

    Search feature & postgres db setting

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from taggit.managers import TaggableManager
 
 
+
 class PublishedManager(models.Manager):
 	def get_queryset(self):
 		return super(PublishedManager, self).get_queryset().filter(status='published')
